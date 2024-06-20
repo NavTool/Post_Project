6bb0a6d56d82bde212d0a1be9c8e30a70d714262
KOROyo123 <1121910079@qq.com>
2024-06-20 18:57:40 +0800

    添加FluentUI脚手架代码

---
+++ src/NavTool_en_US.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="10"/>
+        <source>NavTool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="23"/>
+        <source>Welcome to FluentUI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="28"/>
+        <source>Learn FluentUI</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="43"/>
+        <source>Author&apos;s WeChat ID: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ src/NavTool_zh_CN.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="main.qml" line="10"/>
+        <source>NavTool</source>
+        <translation type="unfinished">NavTool</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="23"/>
+        <source>Welcome to FluentUI</source>
+        <translation type="unfinished">欢迎使用FluentUI</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="28"/>
+        <source>Learn FluentUI</source>
+        <translation type="unfinished">学习FluentUI</translation>
+    </message>
+    <message>
+        <location filename="main.qml" line="43"/>
+        <source>Author&apos;s WeChat ID: </source>
+        <translation type="unfinished">作者微信号: </translation>
+    </message>
+</context>
+</TS>
